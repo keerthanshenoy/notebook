bcf2539dd3c5752c0a82ca6480d7d4e823e6b8f3
keerthanshenoy <keerthan9964@gmail.com>
2020-01-05 10:46:52 +0530

    adding java install shell script

---
+++ checkjavainstall.sh
@@ -0,0 +1,15 @@
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo your current java version is "$version"
+   
+fi
